4b63a31cd3ac89532d687b6c9e2f77ac0e489204
Trần Thái <thaitq.ikong@gmail.com>
2022-05-20 15:45:42 +0700

    Release version 1.0.0

--- app/services/InstagramHelper.js
+++ app/services/InstagramHelper.js
@@ -1,7 +1,7 @@
 class InstagramHelper {
   static BASE_URL = 'https://www.instagram.com/';
   static AUTH_URL = 'https://www.instagram.com/accounts/login/ajax/';
-  static DEFAULT_USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36';
+  static DEFAULT_USER_AGENT = '5.0+(iPhone%3B+CPU+iPhone+OS+14_8+like+Mac+OS+X)+AppleWebKit%2F605.1.15+(KHTML,+like+Gecko)+Version%2F14.1.2+Mobile%2F15E148+Safari%2F604.1';
 
   static #ENCRYPTED_PASSWORD_TEMPLATE = '#PWD_INSTAGRAM_BROWSER:0:%TIME%:%PASSWORD%'
 
